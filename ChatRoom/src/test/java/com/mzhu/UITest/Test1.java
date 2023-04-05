1ac223154167f219a911c2e61fbcec7e44809433
MZKT89 <115808480+MZKT89@users.noreply.github.com>
2023-04-05 23:48:12 +0800

    Delete ChatRoom directory

--- ChatRoom/src/test/java/com/mzhu/UITest/Test1.java
+++
@@ -1,7 +0,0 @@
-package com.mzhu.UITest;
-
-public class Test1 {
-    public static void main(String[] args) {
-        new LoginTest1().setVisible(true);
-    }
-}
--- ChatRoom/src/test/java/openWindowTest1.java
+++
@@ -1,8 +0,0 @@
-import com.mzhu.Windows.UIdesign.loginUI.loginJFrame;
-import com.mzhu.Windows.UIdesign.registerUI.registerJFrame;
-
-public class openWindowTest1 {
-    public static void main(String[] args) {
-        new registerJFrame();
-    }
-}
